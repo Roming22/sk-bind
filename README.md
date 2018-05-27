bff6b4b556469c3eb192a1b1eecd08eb0b3fa69f
Roming22 <roming22@gmail.com>
2018-05-27 15:45:07 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# DNS service for Skwr
+
+## Goal
+Run a DNS server on a local network.
+
+Note: Only the DNS cache has been tested.
+
---
+++ docker/bind/bin/bind.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+SCRIPT_DIR=`cd $(dirname $0); pwd`
+
+// Use opendns by default
+export DNS=${DNS:-opendns}
+
+ETC_DIR=`cd $SCRIPT_DIR/../etc; pwd`
+for SOURCE in `find $ETC_DIR -type f`; do
+	TARGET=`echo $SOURCE | sed "s:$ETC_DIR:/etc/bind:"`
+	[[ ! -e `dirname $TARGET` ]] && mkdir -p `dirname $TARGET`
+	envsubst < $SOURCE > $TARGET
+done
+chown -R bind:bind /etc/bind
+ 
+named -g -u bind
+exit $?
