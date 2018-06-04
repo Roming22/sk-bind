c788afcc519eb0af994659022de3e78909b9dea3
Roming22 <roming22@gmail.com>
2018-06-04 18:10:31 +0000

    Fix configure.sh

--- configure.sh
+++ configure.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR=`cd $(dirname $0); pwd`
 SECRET="$SCRIPT_DIR/etc/secret"
 echo "Choose a DNS in this list:"
 ID=0
-DNS_LIST=`ls $SCRIPT_DIR/app/etc/cache | sed 's:.conf$::'`
+DNS_LIST=`ls $SCRIPT_DIR/docker/bind/etc/cache | sed 's:.conf$::'`
 DNS_LIST=($DNS_LIST)
 for DNS in ${DNS_LIST[@]}; do
 	ID=$((ID+1))
